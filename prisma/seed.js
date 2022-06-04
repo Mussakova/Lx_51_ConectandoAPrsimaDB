c3423f50c4d812630a16d52756a7acdf020a24be
Hirepan E <ginebrasyrel@gmail.com>
2022-06-04 13:32:26 -0500

    Agregando nuevo modelo a schema.prisma y un dato al respectivo modelo

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,61 +3,28 @@ const prisma = new PrismaClient();
 
 (async function main() {
   try {
-    const woopa = await prisma.explorer.upsert({
-      where: { name: 'Woopa' },
-      update: {},
-      create: {
-        name: 'Woopa',
-				username: 'ajolonauta',
-				mission: 'Node'
-      },
-    });
-
-    const woopa1 = await prisma.explorer.upsert({
-      where: { name: 'Woopa1' },
-      update: {},
-      create: {
-        name: 'Woopa1',
-				username: 'ajolonauta1',
-				mission: 'Node'
-      },
-    });
-
-    const woopa2 = await prisma.explorer.upsert({
-      where: { name: 'Woopa 2' },
-      update: {},
-      create: {
-        name: 'Woopa 2',
-				username: 'ajolonauta2',
-				mission: 'Java'
-      },
-    });
-
-    const woopa3 = await prisma.explorer.upsert({
-      where: { name: 'Woopa 3' },
-      update: {},
-      create: {
-        name: 'Woopa 3',
-				username: 'ajolonauta3',
-				mission: 'Node'
-      },
-    });
-
-    const aralya = await prisma.explorer.upsert({
-        where: { name: 'Aralya' },
+      const Explorercrud0 = await prisma.explorerCrud.upsert({
+        where: { name: 'AralyaCrud' },
         update: {},
         create: {
           name: 'Aralya Mussakova',
-                  username: 'Aralya Mssk',
-                  mission: 'Node'
+                  lang: 'Español',
+                  missionCommander: 'Carlogilmar',
+                  enrollments: 567890,
+                  hasCertification: true
         },
       });
 
-    console.log('Create 3 explorers');
-  } catch(e) {
+      
+
+    console.log('Create 3 explorers n 1 Exp.crud');
+  } 
+  
+  catch(e) {
     console.error(e);
     process.exit(1);
   } finally {
     await prisma.$disconnect();
   }
+
 })();
